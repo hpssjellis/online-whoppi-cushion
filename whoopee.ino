fd41d79ab879eff1c5785f3dbf87725214445366
Jeremy Ellis <jellis@hpssapps.com>
2015-01-23 13:04:09 -0800

    Update whoopee.ino

--- whoopee.ino
+++ whoopee.ino
@@ -1,19 +1,19 @@
 
 
 void setup() {
+  pinMode(D7, OUTPUT);      // set Pin to output
   Spark.function("whoopee", myWhoopee); // conect webpage with my function
 }
 
 void loop() {
-   // nothing here
+    delay(1000);            // wait one second = 1000 ms
+    digitalWrite(D7, 0);    // turn off power to D7
+
 }
 
 int myWhoopee(String noArgsNeeded) {  
-    pinMode(D0, OUTPUT);
-    digitalWrite(D0, 1);    // turn on whoopee cushion with 3.3 Volts
-    delay(1000);            // wait one second = 1000 ms
-    digitalWrite(D0, 0);    // turn off power
-  return 1;                 // return that it was successfull
+   digitalWrite(D7, 1);    // turn on D7 or whoopee cushion with 3.3 Volts
+   return 1;               // return that it was successfull
 }
 
 
